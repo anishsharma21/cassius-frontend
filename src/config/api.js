a391b08191aac95edc34be95549b626de2d17135
jasona05 <jastechrnd@gmail.com>
2025-07-31 19:44:32 +0200

    updated api enpoint file

--- src/config/api.js
+++ src/config/api.js
@@ -2,6 +2,8 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const API_ENDPOINTS = {
   login: `${API_BASE_URL}/users/login-get-token`,
+  createUser: `${API_BASE_URL}/users/create-user`,
+  createUserAndCompany: `${API_BASE_URL}/users/create-user-and-company`,
   createUserSocial: `${API_BASE_URL}/users/create-user-social`,
   createCompany: `${API_BASE_URL}/companies/create-company`,
   googleEntry: `${API_BASE_URL}/users/google-entry`,
