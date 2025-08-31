1caa5cd0506c897fb6c5e64d88e0a1d3d47763d3
anishsharma21 <anisharma2103@gmail.com>
2025-08-31 19:37:50 +1000

    whoops, set it correctly

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { PostHogProvider } from 'posthog-js/react'
 
 const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
