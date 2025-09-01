1c6f11727951c87418aa1e86ce00e60d2ffa9fca
anishsharma21 <anisharma2103@gmail.com>
2025-09-02 01:29:22 +1000

    remove ad blocker step in guide

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -121,19 +121,6 @@ const Guide = () => {
             <div className="flex items-start gap-3">
               <input
                 type="checkbox"
-                id="adblock"
-                checked={checkedItems.adblock}
-                onChange={() => toggleCheckItem('adblock')}
-                className="mt-1 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
-              />
-              <label htmlFor="adblock" className="text-base cursor-pointer">
-                Please turn off your Adblock (if you have one) to make sure we can see how you use the platform
-              </label>
-            </div>
-            
-            <div className="flex items-start gap-3">
-              <input
-                type="checkbox"
                 id="profile"
                 checked={checkedItems.profile}
                 onChange={() => toggleCheckItem('profile')}
