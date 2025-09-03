a0abe21d868e4bc98252f6bcecaff44145144a5d
anishsharma21 <anisharma2103@gmail.com>
2025-09-03 12:57:54 +1000

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import PostHogWrapper from './components/PostHogWrapper.jsx'
+import { Analytics } from '@vercel/analytics/next';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PostHogWrapper>
       <App />
+      <Analytics />
     </PostHogWrapper>
   </StrictMode>
 )
